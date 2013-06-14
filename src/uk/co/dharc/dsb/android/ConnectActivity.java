d3e12d68b6ae8b8ca49033461b6e8ae3cd1ae2a3
Nicolas Pope <nwpope@gmail.com>
2013-06-14 16:28:41 +0100

    Initial android project files

---
+++ src/uk/co/dharc/dsb/android/ConnectActivity.java
@@ -0,0 +1,23 @@
+package uk.co.dharc.dsb.android;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ConnectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.connect, menu);
+        return true;
+    }
+    
+}
